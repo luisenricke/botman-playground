52d68ca01c5a7fc717b7728f8912fb4213421c1c
Luis Villalobos <luisevm24@outlook.com>
2020-04-04 01:05:56 -0600

    feat: image attach

--- routes/botman.php
+++ routes/botman.php
@@ -1,5 +1,7 @@
 <?php
 use App\Http\Controllers\BotManController;
+use BotMan\BotMan\Messages\Attachments\Image;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
 
 $botman = resolve('botman');
 
@@ -7,3 +9,20 @@ $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+$botman->hears('image', function (BotMan $bot) {
+    // Create attachment
+    $attachment = new Image('https://botman.io/img/logo.png');
+
+    // Build message object
+    $message = OutgoingMessage::create('This is my text')
+                ->withAttachment($attachment);
+
+    // Reply message object
+    $bot->reply($message);
+});
+
+
+$botman->fallback(function($bot) {
+    $bot->reply('Lo siento, el comando ingresado no esta habilitado');
+});
